72797f2acbf5b323dc519b31b800a224b98470db
guigo <106365467+guigofranca@users.noreply.github.com>
2023-04-21 03:30:26 -0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -43,7 +43,7 @@ int qtdZeros, linhaNula = 0, colunaNula = 0;
             // vamos supor a seguinte matriz > {0, 0} podemos ver a olho que temos uma linha totalmente nula ( 0, 0 );
             //                                 {1, 0}
             // agora faremos um teste de mesa para entender melhor como é percorrido essa condição
-            // começando o for
+            // início do for
             // i = 0;
             // se ( matriz[0][0] == 0 ) faça: quantidade de zeros + 1;
             // olhando para nossa matriz, percebemos que o primeiro valor da matriz é igual a 0, ou seja, vamos somar a quantidade de zeros com + 1;
@@ -51,7 +51,7 @@ int qtdZeros, linhaNula = 0, colunaNula = 0;
             // vamos continuar... ainda no i = 0;
             // se ( matriz[0][1] ) faça: quantidade de zeros + 1;
             // a condição é verdadeira? se sim, soma + 1 na quantidade de zeros;
-            // como a matriz {0, 0} tem apenas duas colunas, o nosso looping for do j terminou; terminamos o nosso for looping com qtdZeros = 2;
+            // como a matriz {0, 0} tem apenas duas colunas, o nosso looping terminou; terminamos o nosso for looping com qtdZeros = 2;
             //               {1, 0}
         }
 
@@ -60,12 +60,12 @@ int qtdZeros, linhaNula = 0, colunaNula = 0;
             linhaNula++;
         }
         // tam = o tamanho da matriz, no exemplo o tamanho é de 2;
-        // qtdZeros = 2, pois é o valor da nosso teste de mesa acima;
+        // qtdZeros = 2, pois é o valor do nosso teste de mesa acima;
 
         // nesse if vamos calcular se a quantidade de zeros é igual ao tamanho da matriz;
         // para a linha ter todos os valores nulos, precisamos que as colunas de uma linha só sejam de valor 0;
-        // ou seja, {0, 0}  linha [0] > valor do j = 0 >> 0, valor do j = 1 >> 0; TODOS OS VALORES DE J É IGUAL A 0, OU SEJA, VAI SER UMA LINHA TODA NULA
-        //          {1, 0}  linha [1] > valor do j = 0 >> 1, valor do j = 1 >> 0; PELO MENOS UM DOS VALORES DE J É DIFERENTE DE 0, OU SEJA, NAO VAI SER UMA LINHA TODA NULA
+        // ou seja, {0, 0}  linha [0] > valor do j sendo [0] é igual a 0, valor do j sendo [1] é igual a 0; TODOS OS VALORES DE J É IGUAL A 0, OU SEJA, VAI SER UMA LINHA TODA NULA
+        //          {1, 0}  linha [1] > valor do j sendo [0] é igual a 1, valor do j sendo [1] é igual a 0; PELO MENOS UM DOS VALORES DE J É DIFERENTE DE 0, OU SEJA, NAO VAI SER UMA LINHA TODA NULA
         // portanto, a primeira linha é uma linha nula, ou seja, linhaNula++
         // a segunda linha não é;
         // quantidade de linhas nulas nessa matriz > 1;
@@ -76,11 +76,11 @@ int qtdZeros, linhaNula = 0, colunaNula = 0;
         qtdZeros = 0; // iniciando sempre o for externo com a quantidade de zeros = 0;
         for ( j = 0; j < tam; j++ ) {
             if ( matriz[j][i] == 0) { // aqui vamos fazer uma condição parecida com a da linha
-                qtdZeros++;           // única diferença é que vamos trocar a matriz[i][j] por matriz[j][i];
+                qtdZeros++;           // a única diferença é que vamos trocar a matriz[i][j] por matriz[j][i];
             }                         // desse jeito vamos calcular quantas colunas nulas existem
         }                             // de resto é igual
-        // ou seja, {0, 0}  coluna [0] > valor do i = 0 >> 0, valor do i = 1 >> 1; // qtdZeros = 1; NÃO É NULA
-        //          {1, 0}  coluna [1] > valor do i = 1 >> 0, valor do j = 1 >> 0; // qtdZeros = 2; É NULA
+        // ou seja, {0, 0}  coluna [0] > valor do i [0] é igual a 0, valor do i = [1] é igual a 1; // qtdZeros = 1; NÃO É NULA
+        //          {1, 0}  coluna [1] > valor do i [1] é igual a 0, valor do j [1] é igual a 0; // qtdZeros = 2; É NULA
         if ( qtdZeros == tam ) {
             colunaNula++; // aqui criamos a variavel colunaNula ao inves de usarmos a linhaNula;
         }
